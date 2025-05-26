13e1841bda9d2cceb741e5e10eac82b03ad0da0c
hoangmaihuy <maihuyhoang97@gmail.com>
2025-05-26 15:19:14 +0700

    refactor: integration test

---
+++ mill-caliban/test/src/io/github/hoangmaihuy/mill/caliban/IntegrationTests.scala
@@ -0,0 +1,27 @@
+package io.github.hoangmaihuy.mill.caliban
+
+import mill.testkit.IntegrationTester
+import utest.*
+
+import scala.runtime.stdLibPatches.Predef.assert
+
+object IntegrationTests extends TestSuite {
+
+  def tests: Tests = Tests {
+    println("initializing mill-caliban.IntegrationTest.tests")
+
+    test("integration") {
+      val resourceFolder = os.Path(sys.env("MILL_TEST_RESOURCE_DIR"))
+      val tester = new IntegrationTester(
+        daemonMode = true,
+        workspaceSourcePath = resourceFolder / "integration",
+        millExecutable = os.Path(sys.env("MILL_EXECUTABLE_PATH"))
+      )
+
+      val res = tester.eval("codegen.compile")
+      println(res.out)
+      println(res.err)
+      assert(res.isSuccess)
+    }
+  }
+}
